939774c9f627aaf973a2f77ad0d3a184c437e537
Choco31415 <Eparke31415@gmail.com>
2016-04-05 10:12:46 -0400

    Added UI.

--- app/src/main/java/com/example/administrator/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworldapp/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.administrator.helloworldapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    Random rng = new Random();
+    int num1;
+    int num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void generateProblem() {
+
+    }{}
 }
